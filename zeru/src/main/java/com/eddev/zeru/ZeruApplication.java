f666d76c3adc547155b60c211c7fedb50794112c
Eduardo Barbosa <eduardocrp@gmail.com>
2020-09-03 23:43:23 -0300

    Recreated application

--- zeru/src/main/java/com/eddev/zeru/ZeruApplication.java
+++ zeru/src/main/java/com/eddev/zeru/ZeruApplication.java
@@ -1,4 +1,4 @@
-package com.eddev.zeru.zeru;
+package com.eddev.zeru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- zeru/src/test/java/com/eddev/zeru/ZeruApplicationTests.java
+++ zeru/src/test/java/com/eddev/zeru/ZeruApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eddev.zeru.zeru;
+package com.eddev.zeru;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

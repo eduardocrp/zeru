961eec3fd8034440f45cd54334e52bffb7e8cfd7
Eduardo Barbosa <eduardocrp@gmail.com>
2020-10-27 12:38:45 -0300

    Created http method's for Phylum

--- zeru/src/main/java/com/eddev/zeru/models/Phylum.java
+++ zeru/src/main/java/com/eddev/zeru/models/Phylum.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.math.BigInteger;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -15,6 +17,7 @@ public class Phylum implements Serializable {
     private static final long serialVersionUID = 1L;
     
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private BigInteger idPhylum;
     private String dsPhylum;
 
--- zeru/src/main/java/com/eddev/zeru/resources/PhylumResource.java
+++ zeru/src/main/java/com/eddev/zeru/resources/PhylumResource.java
@@ -1,6 +1,7 @@
 package com.eddev.zeru.resources;
 
 import java.math.BigInteger;
+import java.net.URI;
 import java.util.List;
 
 import com.eddev.zeru.models.Phylum;
@@ -8,27 +9,63 @@ import com.eddev.zeru.sevices.PhylumService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping(value = "/phylum")
 public class PhylumResource {
-    
+
     @Autowired
     private PhylumService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Phylum> find(@PathVariable BigInteger id){
+    public ResponseEntity<Phylum> find(@PathVariable BigInteger id) {
         return ResponseEntity.ok().body(service.find(id));
     }
 
     @GetMapping
-    public ResponseEntity<List<Phylum>> getMethodName() {
+    public ResponseEntity<List<Phylum>> findAll() {
         return ResponseEntity.ok().body(service.findAll());
     }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody Phylum obj) {
+
+        Phylum phylum = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(phylum.getIdPhylum())
+                .toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@RequestBody Phylum obj) {
+
+        Phylum phylum = service.update(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(phylum.getIdPhylum())
+                .toUri();
+
+        return ResponseEntity.created(uri).build();
+
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable BigInteger id) {
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+        
 
 }

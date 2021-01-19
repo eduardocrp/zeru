29b63311093c38484b7f95163286ae68150f708f
Eduardo Barbosa <eduardocrp@gmail.com>
2021-01-19 16:10:45 -0300

    Add PUBLIC_MATCHERS_GET

--- zeru/src/main/java/com/eddev/zeru/config/SecurityConfig.java
+++ zeru/src/main/java/com/eddev/zeru/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     http.cors().and().csrf().disable;
     http.authorizeRequests()
+        .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
         .antMatchers(PUBLIC_MATCHERS).permitAll()
         .anyRequest().authenticated();
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
